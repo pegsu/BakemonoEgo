c73a2df9c5b9f1f681c98632363d08696b6f8f13
pegsu <kaiser.ps@hotmail.de>
2025-04-28 19:15:23 +0200

    Added styles to main page and question pages

--- BakemonoEgo/MauiProgram.cs
+++ BakemonoEgo/MauiProgram.cs
@@ -13,6 +13,10 @@ namespace BakemonoEgo
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Jersey15-Regular.ttf", "Jersey15Regular");
+                    fonts.AddFont("PixelifySans-Regular.ttf", "PixelifySansRegular");
+                    fonts.AddFont("Tagesschrift-Regular.ttf", "TagesschriftRegular");
+                    fonts.AddFont("Oswald-SemiBold", "OswaldSemiBold");
                 });
 
 #if DEBUG
